f8523a15d9890c118f936992f59a0a2e70e4d85f
dvir <dvir6g6@gmail.com>
2022-05-04 17:30:47 +0300

    storage-service

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {BehaviorSubject, of} from 'rxjs'
 import { map } from 'rxjs/operators';
+import { StorageService } from 'src/app/services/storage.service';
+
 
 
 @Injectable({
@@ -11,21 +13,29 @@ import { map } from 'rxjs/operators';
 export class EventService {
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storageService: StorageService) { }
+
+  private EVENT_KEY: string = "event_db"
 
   private _events$: any = new BehaviorSubject('')
   public events$: any = this._events$.asObservable()
   
   public loadEvents(): void{
-     this._queryEvents().subscribe((res:any)=> this._events$.next(res._embedded.events))
+    let events = this.storageService.loadFromStorage(this.EVENT_KEY)
+    if (!events) {
+     this._queryEvents().subscribe((res:any)=> {
+       this._events$.next(res._embedded.events)
+     this.storageService.saveToStorage(this.EVENT_KEY, res._embedded.events)
+     })
+     return
+    }
+    this.storageService.saveToStorage(this.EVENT_KEY,events)
+    this._events$.next(events)
+      
   }
   
   private _queryEvents(){
-    return of({_embedded:{
-      'events':[{name:'x',url:'y'}]
-    }})
-   
-//  return this.http.get("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=C0BRd57nGzEyWfVUxb5goo7XKwkGdZFA")
+    return this.http.get("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=C0BRd57nGzEyWfVUxb5goo7XKwkGdZFA")
   }
 }
 
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  saveToStorage(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  loadFromStorage(key: string) {
+    let data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : undefined;
+  }
+}
