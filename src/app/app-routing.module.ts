3ea7db2e531582c020ebf1a73638136d17b2e110
dvir <dvir6g6@gmail.com>
2022-05-04 09:07:42 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+  path: '',
+  component: HomePageComponent,
+},
+{
+  path: 'contact',
+  component: ContactPageComponent,
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/cmps/contact-preview/contact-preview.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contact-preview',
+  templateUrl: './contact-preview.component.html',
+  styleUrls: ['./contact-preview.component.scss']
+})
+export class ContactPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() contact: any;
+
+  ngOnInit(): void {
+  }
+
+}
